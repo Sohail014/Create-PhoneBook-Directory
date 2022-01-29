dba59378178e4ddfb143de5e59a652eaeff304af
Mohd Sohail <93813531+Sohail014@users.noreply.github.com>
2022-01-29 09:54:57 +0530

    Add files via upload

---
+++ ContactDetails.java
@@ -0,0 +1,12 @@
+public class ContactDetails {
+
+    public ContactDetails(String landlineNumber, String phoneNUmber) {
+        this.landlineNumber = landlineNumber;
+        this.phoneNUmber = phoneNUmber;
+    }
+
+    String landlineNumber;
+    String phoneNUmber;
+}
+
+
